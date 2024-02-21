3743ce4471a9f17af4454c9f69905b946a39d253
Veronica <veronicaflatto@gmail.com>
2024-02-21 17:36:03 -0500

    Add an API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,9 @@
 // backend/routes/index.js
 const express = require("express");
 const router = express.Router();
+const apiRouter = require("./api");
 
+router.use("/api", apiRouter);
 // Testing
 // router.get("/hello/world", function (req, res) {
 //   res.cookie("XSRF-TOKEN", req.csrfToken());
