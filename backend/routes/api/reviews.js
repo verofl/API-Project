b21c585386f18247a6d1e2e1e01b83e8473e06c8
Veronica <veronicaflatto@gmail.com>
2024-02-27 19:57:39 -0500

    Halfway through Spots routes

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -7,6 +7,47 @@ const {
   Review,
   ReviewImage,
 } = require("../../db/models");
+const { requireAuth } = require("../../utils/auth");
 const router = express.Router();
+
+// Get All Reviews of the Current User
+router.get("/current", requireAuth, async (req, res) => {
+  let allUserReviews = [];
+  const user = req.user.id;
+
+  const allReviews = await Review.findAll({
+    where: {
+      userId: user,
+    },
+    include: [
+      {
+        model: User,
+        attributes: ["id", "firstName", "lastName"],
+      },
+      {
+        model: Spot,
+        attributes: [
+          "id",
+          "ownerId",
+          "address",
+          "city",
+          "state",
+          "country",
+          "lat",
+          "lng",
+          "name",
+          "price",
+        ],
+        include: [SpotImage],
+      },
+      {
+        model: ReviewImage,
+        attributes: ["id", "url"],
+      },
+    ],
+  });
+
+  return res.status(200).json({ Reviews: allReviews });
+});
 
 module.exports = router;
