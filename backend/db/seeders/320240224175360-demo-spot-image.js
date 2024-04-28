f9dc4f68d47e3e690f7748bf2d3b572635476bde
Veronica <veronicaflatto@gmail.com>
2024-04-28 13:06:52 -0400

    removed image id to allow auto-increment

--- backend/db/seeders/320240224175360-demo-spot-image.js
+++ backend/db/seeders/320240224175360-demo-spot-image.js
@@ -13,35 +13,35 @@ module.exports = {
     await SpotImage.bulkCreate([
       {
         // The Town House Preview Image
-        id: 1,
+
         spotId: 1,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1710862807/MythicalBnB%20API%20Project/Town%20House/Preview%20Image/town-house_j5wqvi.jpg",
         preview: true,
       },
       {
         // The Town House Img Detail 1
-        id: 2,
+
         spotId: 1,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1710862826/MythicalBnB%20API%20Project/Town%20House/Spot%20Images/town-house-image3_vjracv.jpg",
         preview: true,
       },
       {
         // The Town House Img Detail 2
-        id: 3,
+
         spotId: 1,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1710862826/MythicalBnB%20API%20Project/Town%20House/Spot%20Images/town-house-image2_yil3q2.jpg",
         preview: true,
       },
       {
         // The Town House Img Detail 3
-        id: 4,
+
         spotId: 1,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1710862827/MythicalBnB%20API%20Project/Town%20House/Spot%20Images/town-house-image4_pojqnv.jpg",
         preview: true,
       },
       {
         // The Town House Img Detail 4
-        id: 5,
+
         spotId: 1,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1710862826/MythicalBnB%20API%20Project/Town%20House/Spot%20Images/town-house-image1_rrazw1.jpg",
         preview: true,
@@ -49,280 +49,280 @@ module.exports = {
 
       {
         // Shadow Estate Preview Image
-        id: 6,
+
         spotId: 2,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1710862811/MythicalBnB%20API%20Project/Shadow%20Estate/Preview%20Image/shadow-estate_mc9uvu.jpg",
         preview: true,
       },
       {
         // Shadow Estate Img Detail 1
-        id: 7,
+
         spotId: 2,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1713998525/MythicalBnB%20API%20Project/Shadow%20Estate/Spot%20Images/shadow-estate-image3_kaavut.jpg",
         preview: true,
       },
       {
         // Shadow Estate Img Detail 2
-        id: 8,
+
         spotId: 2,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1713998526/MythicalBnB%20API%20Project/Shadow%20Estate/Spot%20Images/shadow-estate-image2_kuvope.jpg",
         preview: true,
       },
       {
         // Shadow Estate Img Detail 3
-        id: 9,
+
         spotId: 2,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1713998526/MythicalBnB%20API%20Project/Shadow%20Estate/Spot%20Images/shadow-estate-image1_gtnwuu.jpg",
         preview: true,
       },
       {
         // Shadow Estate Img Detail 4
-        id: 10,
+
         spotId: 2,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1713998528/MythicalBnB%20API%20Project/Shadow%20Estate/Spot%20Images/shadow-estate-image4_daa6xy.jpg",
         preview: true,
       },
       {
         // The House of Night Preview Image
-        id: 11,
+
         spotId: 3,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1710862807/MythicalBnB%20API%20Project/House%20of%20Night/Preview%20Image/house-of-night_znawfo.jpg",
         preview: true,
       },
       {
         // The House of Night  Img Detail 1
-        id: 12,
+
         spotId: 3,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1714002017/MythicalBnB%20API%20Project/House%20of%20Night/Spot%20Images/house-of-night-image3_xlwato.jpg",
         preview: true,
       },
       {
         // The House of Night  Img Detail 2
-        id: 13,
+
         spotId: 3,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1714002018/MythicalBnB%20API%20Project/House%20of%20Night/Spot%20Images/house-of-night-image1_vifcmj.jpg",
         preview: true,
       },
       {
         // The House of Night  Img Detail 3
-        id: 14,
+
         spotId: 3,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1714002019/MythicalBnB%20API%20Project/House%20of%20Night/Spot%20Images/house-of-night-image4_mfkt3l.jpg",
         preview: true,
       },
       {
         // The House of Night  Img Detail 4
-        id: 15,
+
         spotId: 3,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1714002022/MythicalBnB%20API%20Project/House%20of%20Night/Spot%20Images/house-of-night-image2_ee5f7v.jpg",
         preview: true,
       },
       {
         // Moonstone Palace Preview Image
-        id: 16,
+
         spotId: 4,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1710862807/MythicalBnB%20API%20Project/Moonstone%20Palace/Preview%20Image/moonstone-palace_gn1xmr.jpg",
         preview: true,
       },
       {
         // Moonstone Palace Img Detail 1
-        id: 17,
+
         spotId: 4,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1714002064/MythicalBnB%20API%20Project/Moonstone%20Palace/Spot%20Images/moonstone-palace-image2_ycqmru.jpg",
         preview: true,
       },
       {
         // Moonstone Palace Img Detail 2
-        id: 18,
+
         spotId: 4,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1714002066/MythicalBnB%20API%20Project/Moonstone%20Palace/Spot%20Images/moonstone-palace-image1_dfqpyp.jpg",
         preview: true,
       },
       {
         // Moonstone Palace Img Detail 3
-        id: 19,
+
         spotId: 4,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1714002068/MythicalBnB%20API%20Project/Moonstone%20Palace/Spot%20Images/moonstone-palace-image3_exszun.jpg",
         preview: true,
       },
       {
         // Moonstone Palace Img Detail 4
-        id: 20,
+
         spotId: 4,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1714002070/MythicalBnB%20API%20Project/Moonstone%20Palace/Spot%20Images/moonstone-palace-image4_yqszut.jpg",
         preview: true,
       },
       {
         // War College Castle Preview Image
-        id: 21,
+
         spotId: 5,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1710862809/MythicalBnB%20API%20Project/War%20College%20Castle/Preview%20Image/war-college-castle_t6eqk9.jpg",
         preview: true,
       },
       {
         // War College Castle Img Detail 1
-        id: 22,
+
         spotId: 5,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1714002177/MythicalBnB%20API%20Project/War%20College%20Castle/Spot%20Images/war-college-castle-image4_cqndhh.jpg",
         preview: true,
       },
       {
         // War College Castle Img Detail 2
-        id: 23,
+
         spotId: 5,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1714002180/MythicalBnB%20API%20Project/War%20College%20Castle/Spot%20Images/war-college-castle-image1_uxkrwc.jpg",
         preview: true,
       },
       {
         // War College Castle Img Detail 3
-        id: 24,
+
         spotId: 5,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1714002183/MythicalBnB%20API%20Project/War%20College%20Castle/Spot%20Images/war-college-castle-image3_roosvo.jpg",
         preview: true,
       },
       {
         // War College Castle Img Detail 4
-        id: 25,
+
         spotId: 5,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1714002186/MythicalBnB%20API%20Project/War%20College%20Castle/Spot%20Images/war-college-castle-image2_hqy54u.jpg",
         preview: true,
       },
       {
         // Nightfire Mansion Preview Image
-        id: 26,
+
         spotId: 6,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1710862808/MythicalBnB%20API%20Project/Nightfire%20Mansion/Preview%20Image/nightfire-mansion_qcgz92.jpg",
         preview: true,
       },
       {
         // Nightfire Mansion Img Detail 1
-        id: 27,
+
         spotId: 6,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1714258017/MythicalBnB%20API%20Project/Nightfire%20Mansion/Spot%20Images/nightfire-mansion-image2_q8ppzf.jpg",
         preview: true,
       },
       {
         // Nightfire Mansion Img Detail 2
-        id: 28,
+
         spotId: 6,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1714258028/MythicalBnB%20API%20Project/Nightfire%20Mansion/Spot%20Images/nightfire-mansion-image4_seoctj.jpg",
         preview: true,
       },
       {
         // Nightfire Mansion Img Detail 3
-        id: 29,
+
         spotId: 6,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1714258022/MythicalBnB%20API%20Project/Nightfire%20Mansion/Spot%20Images/nightfire-mansion-image1_cbw3yv.jpg",
         preview: true,
       },
       {
         // Nightfire Mansion Img Detail 4
-        id: 30,
+
         spotId: 6,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1714258034/MythicalBnB%20API%20Project/Nightfire%20Mansion/Spot%20Images/nightfire-mansion-image3_xj0hsr.jpg",
         preview: true,
       },
       {
         // River House Preview Image
-        id: 31,
+
         spotId: 7,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1710862810/MythicalBnB%20API%20Project/River%20House/Preview%20Image/river-house_vagpfx.jpg",
         preview: true,
       },
       {
         // River House Img Detail 1
-        id: 32,
+
         spotId: 7,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1714002142/MythicalBnB%20API%20Project/River%20House/Spot%20Images/river-house-image2_mq8kfn.jpg",
         preview: true,
       },
       {
         // River House Img Detail 2
-        id: 33,
+
         spotId: 7,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1714002145/MythicalBnB%20API%20Project/River%20House/Spot%20Images/river-house-image3_hnpu1t.jpg",
         preview: true,
       },
       {
         // River House Img Detail 3
-        id: 34,
+
         spotId: 7,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1714002147/MythicalBnB%20API%20Project/River%20House/Spot%20Images/river-house-image4_qdla5y.jpg",
         preview: true,
       },
       {
         // River House Img Detail 4
-        id: 35,
+
         spotId: 7,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1714002150/MythicalBnB%20API%20Project/River%20House/Spot%20Images/river-house-image1_xd8hug.jpg",
         preview: true,
       },
       {
         // Quaint Home Preview Image
-        id: 36,
+
         spotId: 8,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1710862811/MythicalBnB%20API%20Project/Quaint%20Home/Preview%20Image/quaint-home_l6hbft.jpg",
         preview: true,
       },
       {
         // Quaint Home Img Detail 1
-        id: 37,
+
         spotId: 8,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1714002111/MythicalBnB%20API%20Project/Quaint%20Home/Spot%20Images/quaint-home-image1_c4r2g2.jpg",
         preview: true,
       },
       {
         // Quaint Home Img Detail 2
-        id: 38,
+
         spotId: 8,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1714002114/MythicalBnB%20API%20Project/Quaint%20Home/Spot%20Images/quaint-home-image2_cbraur.jpg",
         preview: true,
       },
       {
         // Quaint Home Img Detail 3
-        id: 39,
+
         spotId: 8,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1714002115/MythicalBnB%20API%20Project/Quaint%20Home/Spot%20Images/quaint-home-image3_rmunxt.jpg",
         preview: true,
       },
       {
         // Quaint Home Img Detail 4
-        id: 40,
+
         spotId: 8,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1714002118/MythicalBnB%20API%20Project/Quaint%20Home/Spot%20Images/quaint-home-image4_vpx2oh.jpg",
         preview: true,
       },
       {
         // Crown of Nyaxia Preview Image
-        id: 41,
+
         spotId: 9,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1710862807/MythicalBnB%20API%20Project/Crown%20of%20Nyaxia/Preview%20Image/crown-of-nyaxia_dq4dwv.jpg",
         preview: true,
       },
       {
         // Crown of Nyaxia Img Detail 1
-        id: 42,
+
         spotId: 9,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1714001979/MythicalBnB%20API%20Project/Crown%20of%20Nyaxia/Spot%20Images/crown-of-nyaxia-image1_jann0g.jpg",
         preview: true,
       },
       {
         // Crown of Nyaxia Img Detail 2
-        id: 43,
+
         spotId: 9,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1714001979/MythicalBnB%20API%20Project/Crown%20of%20Nyaxia/Spot%20Images/crown-of-nyaxia-image4_yxazsy.jpg",
         preview: true,
       },
       {
         // Crown of Nyaxia Img Detail 3
-        id: 44,
+
         spotId: 9,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1714001980/MythicalBnB%20API%20Project/Crown%20of%20Nyaxia/Spot%20Images/crown-of-nyaxia-image2_ybq3ol.jpg",
         preview: true,
       },
       {
         // Crown of Nyaxia Img Detail 4
-        id: 45,
+
         spotId: 9,
         url: "https://res.cloudinary.com/djqcfdrbm/image/upload/v1714001982/MythicalBnB%20API%20Project/Crown%20of%20Nyaxia/Spot%20Images/crown-of-nyaxia-image3_xg0j3c.jpg",
         preview: true,
